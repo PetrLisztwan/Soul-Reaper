c4e5cbf5f33e15a1032f207ad098e34559e30ef1
PetrLisztwan <lisztwan.petr@gmail.com>
2025-12-16 09:48:25 +0000

    SQL first try

--- Assets/Ui Script.cs
+++ Assets/Ui Script.cs
@@ -1,16 +1,16 @@
-using UnityEngine;
-using TMPro;
+using Mono.Data.Sqlite;
+using System.Data;
+using System.Data.SQLite;
 using System.Text.RegularExpressions;
-
+using TMPro;
+using UnityEditor.Search;
+using UnityEngine;
 public class UIScript : MonoBehaviour
 {
     public TMP_InputField Username;
     public TMP_InputField Password;
-
-
+    public bool Passed = false;
     private Regex passwordRegex = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[^\s<>]{8,}$");
-    //private bool Passed = false;
-
 
     public void Enter()
     {
@@ -21,8 +21,8 @@ public class UIScript : MonoBehaviour
             if (passwordRegex.IsMatch(Password.text)) {
                 Debug.Log(Password.text);
                 Debug.Log("Password passed requirement check");
-              
-                //Passed = true;
+                Passed = true;
+          
             }
             else
             {
@@ -35,5 +35,22 @@ public class UIScript : MonoBehaviour
             Debug.Log(Username.text);
             Debug.Log("Username failed requirement check");
         }
+
+        if (Passed == true) {
+
+            string conn = "Data Source=C:\\Users\\s241236\\OneDrive - Greenhead College\\CS Programming\\MasterDatabase.db;Version=3;";
+            using (var connection = new SQLiteConnection(conn))
+            { 
+                connection.Open();
+                using (var command = connection.CreateCommand()) {
+                { 
+                        command.CommandText = "INSERT INTO Users (Username, Password) VALUES (@user, @pass)";
+                        command.ExecuteNonQuery();
+
+                }
+            }
+        }
     }
 }
+
+
