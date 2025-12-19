70b01674518cc816aaa8cf622eb9154d74d670aa
PetrLisztwan <lisztwan.petr@gmail.com>
2025-12-19 10:40:01 +0000

    debug

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private float movementSpeed = 3f;
     private float dodge = 6f;
+    //private float dodgeDuration = 0.15f;
     private Rigidbody2D rb;
     private Vector2 movementDirection;
 
@@ -16,25 +17,20 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(movementDirection * dodge, ForceMode2D.Impulse);
+            Debug.Log("Spacebar pressed down");
+        }
 
     }
+
 
     private void FixedUpdate()
     {
         rb.linearVelocity = movementDirection * movementSpeed;
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.linearVelocity = movementDirection * dodge;
-            Debug.Log("SpaceBar pressed down");
-        }
-        else
-        {
-            rb.linearVelocity = movementDirection * movementSpeed; 
-        }
-
-
     }
 
 }
