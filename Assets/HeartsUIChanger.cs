f1ea24f83f88e13892012eded3a942b4667eecea
PetrLisztwan <lisztwan.petr@gmail.com>
2026-02-27 10:49:29 +0000

    27.2.26

--- Assets/HeartsUIChanger.cs
+++ Assets/HeartsUIChanger.cs
@@ -1,30 +1,25 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HeartsUIChanger : MonoBehaviour
 {
     [SerializeField] private Sprite[] heartSprites;
-    [SerializeField] private PlayerHealth playerHealth; 
-    private SpriteRenderer spriteRenderer;
+    [SerializeField] private PlayerHealth playerHealth;
+    [SerializeField] private Image heartImage;
 
-    void Start()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
     void Update()
     {
-        if (playerHealth == null) return;
+        if (playerHealth == null || heartImage == null) return;
 
-        // Call the method we just added to the Health script
         int currentHP = playerHealth.GetHealth();
 
         if (currentHP <= 0)
-            spriteRenderer.sprite = heartSprites[0];
+            heartImage.sprite = heartSprites[0];
         else if (currentHP < 33)
-            spriteRenderer.sprite = heartSprites[1];
+            heartImage.sprite = heartSprites[1];
         else if (currentHP < 66)
-            spriteRenderer.sprite = heartSprites[2];
+            heartImage.sprite = heartSprites[2];
         else
-            spriteRenderer.sprite = heartSprites[3];
-
+            heartImage.sprite = heartSprites[3];
     }
 }
