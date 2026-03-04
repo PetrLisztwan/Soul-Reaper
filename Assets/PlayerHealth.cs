f67f288cb7af3cb1a6ba0c9e42e2d38fd27c9ecf
PetrLisztwan <lisztwan.petr@gmail.com>
2026-03-04 12:28:08 +0000

    4.3.26 (12:27)

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class PlayerHealth : Health
 {
     protected override void Die()
     {
-        //Time.timeScale = 0; 
         Debug.Log("Player has died!");
         SceneManager.LoadScene("Limbo");
     }
